840e825a9feb215b02a655dbc25c95e20ac7591f
SelimHorri <horri.slim@gmail.com>
2021-10-20 00:00:22 +0100

    add fks migrations

--- src/main/resources/db/migration/V21__create_likes_table.sql
+++ src/main/resources/db/migration/V21__create_likes_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE likes (
 	product_id INT(11) NOT NULL,
 	like_date TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
 	created_at TIMESTAMP DEFAULT LOCALTIMESTAMP NOT NULL NULL_TO_DEFAULT,
-	updated_at TIMESTAMP
+	updated_at TIMESTAMP,
+	PRIMARY KEY (user_id, product_id, like_date)
 );
 
